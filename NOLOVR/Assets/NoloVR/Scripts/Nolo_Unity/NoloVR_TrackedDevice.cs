9979a4bb37488aa3094a5555b7e9657489aa24b2
github@lyrobotix.com <zhenze@lyrobotix.com>
2017-06-05 01:11:18 +0800

    refactor

---
+++ NOLOVR/Assets/NoloVR/Scripts/Nolo_Unity/NoloVR_TrackedDevice.cs
@@ -0,0 +1,41 @@
+﻿/*************************************************************
+ * 
+ *  Copyright(c) 2017 Lyrobotix.Co.Ltd.All rights reserved.
+ *  NoloVR_TrackedDevice.cs
+ *   
+*************************************************************/
+
+using UnityEngine;
+
+public class NoloVR_TrackedDevice : MonoBehaviour {
+
+    public NoloDeviceType deviceType;
+    private GameObject vrCamera;
+    private Vector3 centralPointVector = new Vector3(0, 0.09f, 0.06f);
+    void Start()
+    {
+        vrCamera = NoloVR_Manager.GetInstance().VRCamera;
+    }
+	void Update () {
+        UpdatePose();
+    }
+
+    void UpdatePose()
+    {
+        var pose = NoloVR_Controller.GetDevice(deviceType).GetPose();
+        if (deviceType == NoloDeviceType.Hmd)
+        {
+            if (vrCamera == null)
+            {
+                Debug.LogError("Not find your vr camera");
+                transform.localPosition = pose.pos;
+            }
+            transform.localPosition = pose.pos - vrCamera.transform.localPosition - vrCamera.transform.localRotation * centralPointVector;
+        }
+        else
+        {
+            transform.localPosition = pose.pos;
+            transform.localRotation = pose.rot;
+        }
+    }
+}
